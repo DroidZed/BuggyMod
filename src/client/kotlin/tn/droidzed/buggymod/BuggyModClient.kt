151ff7dfe41577f5fea6f9989558408788683a20
DroidZed <41507665+DroidZed@users.noreply.github.com>
2024-05-25 12:07:47 +0100

    fist commit 🥣

---
+++ src/client/kotlin/tn/droidzed/buggymod/BuggyModClient.kt
@@ -0,0 +1,9 @@
+package tn.droidzed.buggymod
+
+import net.fabricmc.api.ClientModInitializer
+
+object BuggyModClient : ClientModInitializer {
+  override fun onInitializeClient() {
+    // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+  }
+}
---
+++ src/main/kotlin/tn/droidzed/buggymod/BuggyModDataGenerator.kt
@@ -0,0 +1,9 @@
+package tn.droidzed.buggymod
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object BuggyModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
